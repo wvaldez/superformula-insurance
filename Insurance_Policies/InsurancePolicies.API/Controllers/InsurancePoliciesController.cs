b933504797e7f8ae93206c219d860a432ab3abf9
Walterio Valdez <w.valdez@outlook.com>
2022-05-14 17:17:32 -0500

    Add controller

---
+++ Insurance_Policies/InsurancePolicies.API/Controllers/InsurancePoliciesController.cs
@@ -0,0 +1,36 @@
+﻿using InsurancePolicies.Core.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace InsurancePolicies.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InsurancePoliciesController : ControllerBase
+    {
+        public InsurancePoliciesController()
+        {
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<InsurancePolicy>>> Get()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<InsurancePolicy>> Get(int policyId)
+        {
+            return Ok();
+        }
+    }
+}
